0fee95c4c2f3c10c336cdb2b105020c331fd8270
Sahil Patel <spate125@ucr.edu>
2019-08-08 18:58:43 +0100

    5th commit

--- introduction.js
+++ introduction.js
@@ -1,5 +1,9 @@
 function introduction(name) {
-	return "hello my name is " + name;
+	if(name.length > 0) {
+		return "hello my name is " + name;
+	}
+	else {
+		return "hello my name is";
+	}
 }
-
 module.exports = introduction;
